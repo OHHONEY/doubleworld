9a471e63f199a915b1c8378c182825990c7def4e
chenxi <457903794@qq.com>
2018-11-21 10:21:21 +0800

    设置四行缩进

--- src/App.vue
+++ src/App.vue
@@ -21,3 +21,9 @@ export default {
   }
 };
 </script>
+
+<style lang="less">
+p {
+  color: chocolate;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        //please import this plugin
-        new VueLoaderPlugin()
-    ],
-    module: {
-        rules: [{
-            test: /\.vue$/,
-            loader: 'vue-loader'
-        }, {
-            test: /\.less$/,
-            use: [
-                'vue-style-loader',
-                'css-loader',
-                'less-loader'
-            ]
-        }]
-    }
+	entry: './src/main.js',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		//please import this plugin
+		new VueLoaderPlugin(),
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
+	module: {
+		rules: [{
+			test: /\.vue$/,
+			loader: 'vue-loader'
+		}, {
+			test: /\.less$/,
+			use: [
+				'vue-style-loader',
+				'css-loader',
+				'less-loader'
+			]
+		}]
+	}
 }
